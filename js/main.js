5797765e8b3993d13057b4e82e6409971269a51e
Bueno-Hugo <hbueno5000@gmail.com>
2022-01-15 19:51:35 -0300

    version joyeria-estable

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const altura =  document.body.scrollHeight - window.innerHeight;
 const fondo = document.getElementById('fondo');
 
 window.onscroll =  ()  =>  {
-        const  anchoFondo  =  (window.pageYOffset  / altura)  * 600;
+        const  anchoFondo  =  (window.pageYOffset  / altura)  * 900;
          if (anchoFondo <= 100)  {
             fondo.style.width  =  anchoFondo  + '%';
         }
